712bd88c930c5cabf54d0c647bc2203d4647bb76
Weboviny Deploy <deploy@weboviny.cz>
2026-09-25 09:41:33 +0200

    Deploy 2026-09-25

---
+++ _next/static/chunks/app/balicky/page-11e548a6afcc490a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[54],{1003:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.bind(n,7519))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,374,519,441,517,358],()=>s(1003)),_N_E=e.O()}]);
